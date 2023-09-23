b222e8c17b246f71e47c72d7706516a134f93d65
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2023-09-23 16:57:54 -0400

    updated middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,24 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = {
+  load: {
+    before: [
+      'strapi::errors',
+      'strapi::security',
+      'strapi::cors',
+      'strapi::poweredBy',
+      'strapi::logger',
+      'strapi::body',
+      'strapi::session',
+      'strapi::favicon',
+      'strapi::query'
+    ],
+    after: ['strapi::public'],
+  },
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['https://sea-moss-sea-moss-bros.vercel.app', 
+        'https://sea-moss-git-main-sea-moss-bros.vercel.app',
+      ]
+    },
+  },
+};
