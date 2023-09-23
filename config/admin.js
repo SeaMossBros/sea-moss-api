67b9a669bbe289343eae388cef9ade419fb531c1
Ras Ally <rasboss@rass-air.myfiosgateway.com>
2023-09-23 12:43:56 -0400

    add keys and data

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,15 @@
+const crypto = require('crypto');
+
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET') || crypto.randomBytes(32).toString('hex'), // Generate a random secret if not provided
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT') || crypto.randomBytes(16).toString('base64'), // Generate a random salt if not provided
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT') || crypto.randomBytes(16).toString('base64'), // Generate a random salt if not provided
     },
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
