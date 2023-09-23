cf001836b823cb4501a6a52fb3e1014df99225bd
Ras Ally <rasboss@rass-air.myfiosgateway.com>
2023-09-23 16:28:34 -0400

    add working backend

--- config/admin.js
+++ config/admin.js
@@ -1,15 +1,13 @@
-const crypto = require('crypto');
-
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET') || crypto.randomBytes(32).toString('hex'), // Generate a random secret if not provided
+    secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT') || crypto.randomBytes(16).toString('base64'), // Generate a random salt if not provided
+    salt: env('API_TOKEN_SALT'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT') || crypto.randomBytes(16).toString('base64'), // Generate a random salt if not provided
+      salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
 });
