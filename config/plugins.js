a27c2b4215fe77b75058c8f1aa68a17b0c4a562b
Hoan Than <hoanthan317@gmail.com>
2023-12-10 23:26:11 +0700

    feat: integrate fuzzy search

--- config/plugins.js
+++ config/plugins.js
@@ -18,5 +18,35 @@ module.exports = {
   },
   'schemas-to-ts': {
     enabled: true,
+  },
+  'fuzzy-search': {
+    enabled: true,
+    config: {
+      contentTypes: [
+        {
+          uid: "api::author.author",
+          modelName: "article",
+          fuzzysortOptions: {
+            characterLimit: 500,
+            threshold: -1000,
+            limit: 15,
+            keys: [
+              {
+                name: "title",
+                weight: 1,
+              },
+              {
+                name: "introduction",
+                weight: 1,
+              },
+              {
+                name: 'content',
+                weight: 2
+              }
+            ],
+          },
+        },
+      ],
+    },
   }
 }
