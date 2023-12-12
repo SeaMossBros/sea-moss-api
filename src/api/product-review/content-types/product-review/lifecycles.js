6354fa0a5d4afeafc90267bf7b5f227b956f8b1b
Hoan Than <hoanthan317@gmail.com>
2023-12-12 20:43:04 +0700

    feat: re-calculate product rating after update review

--- src/api/product-review/content-types/product-review/lifecycles.js
+++ src/api/product-review/content-types/product-review/lifecycles.js
@@ -1,4 +1,9 @@
-const updateProductRatingAfterUpsert = async (productId, review) => {
+/**
+ * 
+ * @param {number} productId 
+ * @param {import("./product-review").ProductReview_Plain} review 
+ */
+const updateProductRatingAfterCreate = async (productId, review) => {
   const [reviewsCount, product] = await Promise.all([
     strapi.entityService.count('api::product-review.product-review', {
       filters: {
@@ -24,6 +29,14 @@ const updateProductRatingAfterUpsert = async (productId, review) => {
   })
 }
 
+/**
+ * 
+ * @param {number} productId 
+ * @param {{
+ * count: number
+ * rating: number
+ * }} state 
+ */
 const updateProductRatingAfterDelete = async (productId, state) => {
   const [reviewsCount, product] = await Promise.all([
     strapi.entityService.count('api::product-review.product-review', {
@@ -50,13 +63,51 @@ const updateProductRatingAfterDelete = async (productId, state) => {
   })
 }
 
+/**
+ * 
+ * @param {number} productId 
+ * @param {number[]} prevRatings 
+ * @param {number[]} newRatings 
+ */
+const updateProductRatingAfterUpdate = async (productId, prevRatings, newRatings) => {
+  console.log({
+    productId,
+    prevRatings,
+    newRatings
+  })
+
+  const [reviewsCount, product] = await Promise.all([
+    strapi.entityService.count('api::product-review.product-review', {
+      filters: {
+        product: {
+          id: productId
+        },
+      }
+    }),
+    strapi.entityService.findOne('api::product.product', productId)
+  ])
+
+  const currentProductRating = product.rating ?? 0
+  const previousSumRatings = currentProductRating * reviewsCount
+  const productSumRatingWithoutAffectedEntries = previousSumRatings - prevRatings.reduce((sum, rating) => sum + rating, 0)
+  const newProductSumRatingWithNewAffactedEntries = productSumRatingWithoutAffectedEntries + newRatings.reduce((sum, rating) => sum + rating, 0)
+
+  const newRating = newProductSumRatingWithNewAffactedEntries / reviewsCount
+
+  await strapi.entityService.update('api::product.product', productId, {
+    data: {
+      rating: newRating
+    }
+  })
+}
+
 module.exports = {
   afterCreate(event) {
     const { result, params: {
       data
     } } = event
 
-    updateProductRatingAfterUpsert(data.product, result)
+    updateProductRatingAfterCreate(data.product, result)
   },
   async beforeDelete(event) {
     const { params: {
@@ -111,5 +162,28 @@ module.exports = {
     }
 
     updateProductRatingAfterDelete(productId, state)
+  },
+  async beforeUpdate(event) {
+    const { params: {
+      where: {
+        id
+      }
+    } } = event
+
+    const review = await strapi.entityService.findOne('api::product-review.product-review', id, {
+      populate: ['product']
+    })
+
+    event.state.review = review
+  },
+  async afterUpdate(event) {
+    const { result, state: {
+      review
+    } } = event
+
+    const prevRatings = [review.rating]
+    const newRatings = [result.rating]
+
+    updateProductRatingAfterUpdate(review.product.id, prevRatings, newRatings)
   }
 }
