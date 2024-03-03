0baa9f83d13a6013600285e8ab4eb742495aadfc
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2024-03-03 14:35:08 -0500

    created user after paying with stripe if none exists

--- src/api/order/controllers/payment.js
+++ src/api/order/controllers/payment.js
@@ -17,8 +17,8 @@ module.exports = {
       }
 
       /**
-           * @type {any}
-           */
+      * @type {any}
+      */
       const matchedOrders = await strapi.entityService.findMany('api::order.order', {
         filters: {
           payment_session_id: session.id,
@@ -27,8 +27,8 @@ module.exports = {
         populate: ['cart']
       })
       /**
-       * @type {import("../content-types/order/order").Order_Plain}
-       */
+      * @type {import("../content-types/order/order").Order_Plain}
+      */
       const order = matchedOrders[0]
       if (!order) throw new Error(`Order with payment session ID = '${ session.id }' not found`)
 
@@ -37,7 +37,7 @@ module.exports = {
           /**
            * @type {any}
            */
-          const customer = await stripe.customers.retrieve(session.customer)
+          const customer = await stripe.customers.retrieve(session.customer.toString())
           const updatedOrder = await strapi.entityService.update('api::order.order', order.id, {
             data: {
               payment_status: 'success',
@@ -45,13 +45,49 @@ module.exports = {
               publishedAt: new Date()
             }
           })
+
+          // Check if the email is already in use
+          let existingUser = await strapi.query('plugin::users-permissions.user').findOne({
+            where: { email: customer.email },
+          });
+
+          if (!existingUser) {
+            function generatePassword(length = 12) {
+              const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+              let password = '';
+              for (let i = 0; i < length; i++) {
+                const randomIndex = Math.floor(Math.random() * charset.length);
+                password += charset[randomIndex];
+              }
+              return password;
+            }
+
+            // Example usage:
+            const password = generatePassword(16); 
+
+            // const newStrapiCustomer = await strapi.auth.register();
+            // Create the user
+            existingUser = await strapi.plugins['users-permissions'].services.user.add({
+              username: customer.email.split('@')[0],
+              email: customer.email ,
+              password,
+              provider: 'local',
+              role: 1
+            });
+            existingUser = { ...existingUser, password }
+          }
+
+          console.log('existingUser', existingUser);
+
           responseData.order = updatedOrder
           strapi.entityService.update('api::cart.cart', order.cart.id, {
             data: {
               is_checked_out: true,
             }
           })
-          break
+
+          ctx.body = JSON.stringify({ ...responseData, user: existingUser });
+          return;
         }
         case 'expired': {
           const updatedOrder = await strapi.entityService.update('api::order.order', order.id, {
@@ -73,7 +109,7 @@ module.exports = {
           break
       }
 
-      ctx.body = JSON.stringify(responseData);
+      ctx.body = JSON.stringify({ ...responseData, user: null});
     } catch (err) {
       ctx.body = err;
     }
