51374c1bfdfe5764e19bbe034dfd637b0b04c8b2
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2024-03-03 20:20:33 -0500

    updated config

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'seathemoss'),
   port: env.int('PORT', 1337),
-  url: env('', 'http://0.0.0.0:1337'), 
+  url: env('SERVER_URL', 'https://seathemoss:1337'), 
   app: {
     keys: env.array('APP_KEYS'),
   },
